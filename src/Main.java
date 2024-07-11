09444814bcbc07c30e47dce722fa85da09281ae1
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-07-11 00:48:57 -0500

    coneccion e insercion

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String url = "jdbc:mysql://localhost:3306/Poo";
+        String user = "root";
+        String password = "";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String sql = "INSERT INTO estudiantes (cedula, nombre, b1, b2) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            PreparedStatement cadenaPreparada = connection.prepareStatement(sql);
+            String cedula = JOptionPane.showInputDialog(null, "Introduzca la cédula");
+            String nombre = JOptionPane.showInputDialog(null, "Introduzca el nombre");
+            String b1Str = JOptionPane.showInputDialog(null, "Introduzca la nota de b1");
+            String b2Str = JOptionPane.showInputDialog(null, "Introduzca la nota de b2");
+
+            Double b1 = Double.parseDouble(b1Str);
+            Double b2 = Double.parseDouble(b2Str);
+
+            Estudiante estudiante = new Estudiante(cedula, nombre, b1, b2);
+
+            cadenaPreparada.setString(1, estudiante.getCedula());
+            cadenaPreparada.setString(2, estudiante.getNombre());
+            cadenaPreparada.setDouble(3, estudiante.getB1());
+            cadenaPreparada.setDouble(4, estudiante.getB2());
+
+            cadenaPreparada.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Datos insertados", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error ", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
