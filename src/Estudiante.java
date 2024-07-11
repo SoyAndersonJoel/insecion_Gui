4b7aa513be4940bd38ad002a38a6fb7b63962c8b
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-07-11 00:45:16 -0500

    clase estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,2 +1,30 @@
+
 public class Estudiante {
+    private String cedula;
+    private String nombre;
+    private Double b1;
+    private Double b2;
+
+    public Estudiante(String cedula, String nombre, Double b1, Double b2) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.b1 = b1;
+        this.b2 = b2;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getB1() {
+        return b1;
+    }
+
+    public Double getB2() {
+        return b2;
+    }
 }
